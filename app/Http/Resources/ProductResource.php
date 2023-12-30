f761caa83362768f73cde678102e9cd4b57e266f
Hamad Adel <hamadwebdeveloper@gmail.com>
2023-12-30 17:14:32 +0200

    created ProductResource which merge the ProductIndexResource array with Product Resource into one more detailed

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends ProductIndexResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+        return array_merge(parent::toArray($request), [
+            'variations' => []
+        ]);
+    }
+}
