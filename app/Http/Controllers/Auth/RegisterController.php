081899aee9b9ce9cf29ff8496ddcd443dc87b4f1
Hamad Adel <hamadwebdeveloper@gmail.com>
2024-01-09 00:03:38 +0200

    modify RegisterController to single action controller which is register a new user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Http\Resources\PrivateUserResource;
 
 class RegisterController extends Controller
 {
+    /**
+     * Handle the incoming request.
+     */
     public function __invoke(RegisterRequest $request)
     {
-        dd($request);
         $user = User::create($request->only('email', 'name', 'password'));
         return new PrivateUserResource($user);
     }
