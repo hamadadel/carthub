553db117281ae811c90546469723788f245ea2bc
Hamad Adel <hamadwebdeveloper@gmail.com>
2024-01-10 16:48:10 +0200

    add route group with prefix 'auth' for reigster and login routes

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Categories\CategoryController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Products\ProductController;
+use App\Http\Controllers\Categories\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -25,4 +26,5 @@ Route::resource('/products', ProductController::class);
 Route::group(['prefix' => 'auth'], function () {
 
     Route::post('register', RegisterController::class);
+    Route::post('login', LoginController::class);
 });
